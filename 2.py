83548d4491a77ba517692aca59545deb6dfca009
Александр Мякишев <myakishev.av@mse.local>
2016-12-27 14:39:34 +0600

    -

--- 2.py
+++ 2.py
@@ -4,19 +4,19 @@ import screen, sys, os
 app=QtGui.QApplication(sys.argv)
 window=QtGui.QWidget()
 ui=screen.Ui_Dialog()
-ui.setup(window)
+ui.setupUi(window)
 
-QtCore.QObject.connect(ui.pushButton,QtCore.SIGNAL("clicked"),lambda:test(ui))
+QtCore.QObject.connect(ui.pushButton,QtCore.SIGNAL("clicked()"),lambda:test(ui))
 
 def test(ui):
 	dir=os.getcwd()+"/screenhot"
 	path=dir+"/screenhot.jpg"
 	if not os.path.exists(dir):
 		os.makedirs(dir)
-	QtGui.QPixmap.qrabWindow(QtGui.QApplication.desktop().winId()).save(path,'jpg')
+	QtGui.QPixmap.grabWindow(QtGui.QApplication.desktop().winId()).save(path,'jpg')
 	pic=ui.label
 	pic.setGeometry(10,10,450,300)
-	pic.setPixmap(QtGui,QPixmap(path))
+	pic.setPixmap(QtGui.QPixmap(path))
 	pic.setScaledContents(True)
 
 window.show()
