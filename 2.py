07eaec69287eb0d3234c5b25960f76fc1462ae14
Александр Мякишев <myakishev.av@mse.local>
2016-12-27 14:04:36 +0600

    код скрина

---
+++ 2.py
@@ -0,0 +1,23 @@
+from PyQt4 import QtCore, QtGui
+import screen, sys, os
+
+app=QtGui.QApplication(sys.argv)
+window=QtGui.QWidget()
+ui=screen.Ui_Dialog()
+ui.setup(window)
+
+QtCore.QObject.connect(ui.pushButton,QtCore.SIGNAL("clicked"),lambda:test(ui))
+
+def test(ui):
+	dir=os.getcwd()+"/screenhot"
+	path=dir+"/screenhot.jpg"
+	if not os.path.exists(dir):
+		os.makedirs(dir)
+	QtGui.QPixmap.qrabWindow(QtGui.QApplication.desktop().winId()).save(path,'jpg')
+	pic=ui.label
+	pic.setGeometry(10,10,450,300)
+	pic.setPixmap(QtGui,QPixmap(path))
+	pic.setScaledContents(True)
+
+window.show()
+sys.exit(app.exec_())
